8674021206915eb4ef941de2947311689b180418
mmitov <mihail.mitov@droxic.com>
2024-01-28 15:23:25 +0200

    chore(fe): setup tailwind themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-
+import { useEffect, useState } from "react";
 import "./App.css";
 
+
 function App() {
+  const [darkMode, setdarkMode] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      // document.documentElement.classList.add("dark");
+      document.documentElement.dataset.theme = "dark";
+    } else {
+      document.documentElement.dataset.theme = "";
+    }
+  }, [darkMode]);
 
   return (
     <>
-      <h1 className="text-xl font-bold text-red-500">Tailwindcss setup</h1>
+      {/* <h1 className="text-xl font-bold text-red-500">Tailwindcss setup</h1> */}
+      <button
+        className="bg-bkg text-content "
+        onClick={() => {
+          setdarkMode((v) => !v);
+        }}
+      >
+        change theme {darkMode ? "dark" : "white"}
+      </button>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          1: "rgba(var(--color-accent1) / <alpha-value>)",
+          2: "rgba(var(--color-accent2) / <alpha-value>)",
+        },
+        bkg: "rgba(var(--color-bkg) / <alpha-value>)",
+        content: "rgba(var(--color-content) / <alpha-value>)",
+      },
+      animation: {
+        "spin-slower": "spin 35s ease infinite",
+        "spin-slow": "spin 25s ease-in-out infinite reverse",
+      },
+    },
   },
   plugins: [],
 }
