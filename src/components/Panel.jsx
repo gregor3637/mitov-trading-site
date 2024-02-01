51f28b576ced8aff9c0cef65cb71f86d2fff425e
mmitov <mihail.mitov@droxic.com>
2024-02-01 13:54:44 +0200

    feat(fe): create 'new investment' button in investment page

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -3,17 +3,28 @@ import React from 'react'
 const Panel = ({ children, className, title }) => {
     return (
         <div
-            className={`${className} rounded-xl border-2 border-[--panel-border] bg-gradient-to-r from-[--investments-bg-from] to-[--investments-bg-to] p-4 
+            className={`${className} overflow-hidden rounded-xl border-2 border-[--panel-border] bg-gradient-to-r from-[--investments-bg-from] to-[--investments-bg-to]
             `}
         >
-            <h1
-                className="pb-4 text-center text-3xl font-bold text-[--text-color]
+            <div className=" mb-2 flex items-center justify-between border-b-2 border-[--edge] px-10 py-2">
+                <h1
+                    className=" text-center text-3xl font-bold text-[--text-color] 
                  xl:text-start"
-            >
-                {title}
-            </h1>
+                >
+                    {title}
+                </h1>
+                <button
+                    className="flex items-center gap-2 rounded-[--primary-button-round] border-2 border-dashed border-[--primary-button-borderColor] bg-[--primary-button-color] px-4 py-2 text-[--primary-button-textColor]
+                    hover:bg-blue-400 hover:border-transparent
+                    md:px-2"
+                >
+                    <span className="leading-1 text-3xl ">+</span>{' '}
+                    <span className="hidden md:block ">New</span>{' '}
+                    <span className="hidden lg:block">Investment</span>
+                </button>
+            </div>
             <div
-                className="flex flex-col gap-2 lg:flex-row lg:gap-4 
+                className="m-4 flex flex-col gap-2 lg:flex-row lg:gap-4
                 xl:gap-10"
             >
                 {children}
