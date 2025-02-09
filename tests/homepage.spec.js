b7e928913817f12ac88d81438dab2764d6bdf9db
Mihal Mitov <m.mitov@dynamosoftware.com>
2025-02-09 22:56:28 +0200

    argos + local screenshots

---
+++ tests/homepage.spec.js
@@ -0,0 +1,11 @@
+// tests/example.spec.ts
+import { test, expect } from '@playwright/test';
+import { argosScreenshot } from "@argos-ci/playwright";
+
+
+test('should work', async ({ page }) => {
+  await page.goto('https://www.example.com');
+  await argosScreenshot(page, "This is title - homepage 1");
+  await expect(page).toHaveTitle(/Example Domain/);
+  await argosScreenshot(page, "This is title - homepage 2");
+});
