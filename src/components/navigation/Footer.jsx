c0d33aaa4a478ce39b21876d18b92f7b6bf418e7
Mihal Mitov <m.mitov@dynamosoftware.com>
2025-02-09 23:22:33 +0200

    footer

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[-body-color]  text-center py-2 text-2xl text-[--text-color] border-t-2 border-[--edge]'>All Rights Reserved</div>
+    <div className='bg-[-body-color]  text-center py-2 text-2xl text-[--text-color] border-t-2 border-[--edge]'>All Rights Reserved 22s</div>
   )
 }
 
