c9d924fd6dbc8019c587ae1f30f6c5cfddc91714
Mihal Mitov <m.mitov@dynamosoftware.com>
2025-02-09 22:36:33 +0200

    argos screenshots

--- playwright.config.js
+++ playwright.config.js
@@ -3,5 +3,25 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests', // Directory where your tests are located
   // ... other configuration options
-  
+  reporter: [
+    // Use "dot" reporter on CI, "list" otherwise (Playwright default).
+    process.env.CI ? ["dot"] : ["list"],
+    // Add Argos reporter.
+    [
+      "@argos-ci/playwright/reporter",
+      {
+        // Upload to Argos on CI only.
+        uploadToArgos: !!process.env.CI,
+      },
+    ],
+  ],
+
+  // Setup recording option to enable test debugging features.
+  use: {
+    // Collect trace when retrying the failed test.
+    trace: 'on-first-retry',
+
+    // Capture screenshot after each test failure.
+    screenshot: "only-on-failure",
+  }
 });
